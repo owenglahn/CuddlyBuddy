449aa47897627a03e7f106ed9b6c40ab102b96ae
Liam Ladougne <liamladougne@Liams-MacBook-Pro.local>
2021-01-30 17:14:25 -0500

    added links to each bot

--- app.py
+++ app.py
@@ -10,10 +10,37 @@ training_data = open('training/data.txt').read().splitlines()
 #trainer.train("chatterbot.corpus.english") #train the chatter bot for english
 trainer = ListTrainer(englishBot)
 trainer.train(training_data)
+
 #define app routes
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/cat")
+def cat():
+    return render_template("cat.html")
+
+@app.route("/dog")
+def dog():
+    return render_template("dog.html")
+
+@app.route("/sloth")
+def sloth():
+    return render_template("sloth.html")
+
+@app.route("/penguin")
+def penguin():
+    return render_template("penguin.html")
+
+@app.route("/panda")
+def panda():
+    return render_template("panda.html")
+
+@app.route("/bunny")
+def bunny():
+    return render_template("bunny.html")
+
+
 @app.route("/get")
 #function for the bot response
 def get_bot_response():
